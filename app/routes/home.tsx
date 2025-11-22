f30ecab7d93f8078e89afa5fa8be0e789698284c
Matias Alvarez <117111792+Mattcab98@users.noreply.github.com>
2025-11-22 22:39:42 +0000

    feat: add section hero

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,12 @@
 import "../app.css";
 import Header from "../components/header/Header";
-
+import Hero from '../components/hero/Hero'
 
 export default function Home() {
   return (
     <>
         <div>
+          <Hero/>
         </div>
     </>
   );
