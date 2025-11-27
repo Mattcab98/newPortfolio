0f5ca068948d1043e3187e1529c9764fe640f1cf
Matias Alvarez <117111792+Mattcab98@users.noreply.github.com>
2025-11-27 23:03:51 +0000

    style(card): apply general style updates to the CardProyect component

--- app/components/proyects/CardProyect.tsx
+++ app/components/proyects/CardProyect.tsx
@@ -16,19 +16,19 @@ const stackTech = [
 export default function CardProyect ({imgProyect, title, description, tecnologias}: CardProps) {
     return (
         <>
-            <div className="container__card bg-gray-500 w-[250px] h-[250px] rounded-2xl hover:border hover:border-color-red-600">
+            <div className="container__card bg-gray-900 w-[250px] h-[250px] rounded-2xl border-2 border-transparent hover:border-blue-950 transition-border duracion-1000">
                 <div className="container__img w-full h-[120px]">
                     <img src={imgProyect} alt="" className="w-full h-full 
-                    rounded-t-xl object-cover filter brightness-50 active:brightness-100 hover:brightness-100 transition duration-300"/>
+                    rounded-t-2xl object-cover filter brightness-50 active:brightness-100 hover:brightness-100 transition duration-300"/>
                 </div>
-                <div className="container__txt w-full h-full flex flex-col justify-center">
-                    <h3>{title}</h3>
-                    <p>{description}</p>
-                    <ul className="container__tecnologias">
+                <div className="container__txt flex flex-col flex-1 justify-center aling-center  text-center">
+                    <h3 className="text-sm">{title}</h3>
+                    <p className="text-xs">{description}</p>
+                    <ul className="container__tecnologias flex justify-center gap-5">
                         {stackTech
                             .filter(t => tecnologias.includes(t.id))
                             .map(t => (
-                                <li key={t.id}>{t.label}</li>
+                                <li key={t.id} className="text-[.5rem] p-1 rounded bg-red-600">{t.label}</li>
                             ))
                         }
                     </ul>
--- app/components/proyects/Proyects.tsx
+++ app/components/proyects/Proyects.tsx
@@ -4,17 +4,39 @@ import CardProyect from "./CardProyect";
 export default function Proyects () {
     return (
         <>
-            <h2>Proyectos Destacados</h2>
-            <span>Algunos de nuestros trabajos</span>
-            <div className="bg-red-400 p-5">
-                <CardProyect
-                imgProyect="/siteVicArroche.png"
-                title="Porfolio Profesional Fotografia"
-                description="Este es un proyecto de ejemplo que muestra las capacidades de React."
-                tecnologias={['react']}
-            />
-            </div>
+            <div className="bg-gray-850 w-full flex flex-col ">
+                <div className="container__txt text-center">
+                    <h2>Proyectos Destacados</h2>
+                    <span>Algunos de nuestros trabajos</span>
+                </div>
+                <div className="w-100 grid grid-cols-1 lg:grid-cols-3 auto-rows-auto place-items-center">
+                    <CardProyect
+                        imgProyect="/siteVicArroche.png"
+                        title="Porfolio Profesional Fotografia"
+                        description="Este es un proyecto de ejemplo que muestra las capacidades de React."
+                        tecnologias={['react', 'tailwind', 'node']}
+                    />
+                    <CardProyect
+                        imgProyect="/siteVicArroche.png"
+                        title="Porfolio Profesional Fotografia"
+                        description="Este es un proyecto de ejemplo que muestra las capacidades de React."
+                        tecnologias={['react', 'tailwind', 'node']}
+                    />
+                    <CardProyect
+                        imgProyect="/siteVicArroche.png"
+                        title="Porfolio Profesional Fotografia"
+                        description="Este es un proyecto de ejemplo que muestra las capacidades de React."
+                        tecnologias={['react', 'tailwind', 'node']}
+                    />
+                    <CardProyect
+                        imgProyect="/siteVicArroche.png"
+                        title="Porfolio Profesional Fotografia"
+                        description="Este es un proyecto de ejemplo que muestra las capacidades de React."
+                        tecnologias={['react', 'tailwind', 'node']}
+                    />
+                </div>
 
+            </div>
         </>
     )
 }
